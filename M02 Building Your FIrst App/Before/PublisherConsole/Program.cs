80f62db3e6d81a4ebae389fd781836e69e60d207
Chris Taylor <chrisjtaylor@comcast.net>
2025-08-24 16:54:37 -0600

    Completed Module 2 - Switched to using SQL Server 2022 instead of LocalDb - Initial creation, population of Book and Author

--- M02 Building Your FIrst App/Before/PublisherConsole/Program.cs
+++ M02 Building Your FIrst App/Before/PublisherConsole/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherData;
+using PublisherDomain;
+
+// Following is useful for demos only
+using PubContext context = new();
+context.Database.EnsureCreated();
+Console.WriteLine("Database created (if it did not exist).");
+
+//AddAuthor();
+//GetAuthors();
+
+//void AddAuthor()
+//{
+//    var author = new Author
+//    {
+//        FirstName = "Chris",
+//        LastName = "Taylor"
+//    };
+//    context.Authors.Add(author);
+//    context.SaveChanges();
+//}
+
+//void GetAuthors()
+//{
+//    var authors = context.Authors.ToList();
+//    foreach (var author in authors)
+//    {
+//        Console.WriteLine($"{author.Id}: {author.FirstName} {author.LastName}");
+//    }
+//}
+
+AddAuthorWithBook();
+GetAuthorsWithBooks();
+
+void AddAuthorWithBook()
+{
+    var author = new Author { FirstName = "Julie", LastName = "Lerman" };
+    author.Books.Add(new Book
+    {
+        Title = "Programming Entity Framework",
+        PublishDate = new DateOnly(2009, 1, 1)
+    });
+    author.Books.Add(new Book
+    {
+        Title = "Programming Entity Framework 2nd Ed",
+        PublishDate = new DateOnly(2010, 8, 1)
+    });
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+void GetAuthorsWithBooks()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.Include(a => a.Books).ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+        foreach (var book in author.Books)
+        {
+            Console.WriteLine(book.Title);
+        }
+    }
+}
---
+++ M02 Building Your FIrst App/Before/PublisherData/PubContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=PubIntroDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
